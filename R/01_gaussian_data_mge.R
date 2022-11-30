f573dbc4b675585ffc701808b6f535cfc253575d
Joel Pick <joel.l.pick@gmail.com>
2022-11-30 16:32:55 +0000

    some other scripts

---
+++ R/01_gaussian_data_mge.R
@@ -0,0 +1,176 @@
+
+rm(list=ls())
+
+
+library(asreml)
+library(parallel)
+
+wd <- "/Users/joelpick/github/maternal_effects/"
+
+data_wd <- paste0(wd,"Data/Intermediate/")
+
+source(paste0(wd,"R/extract_cousins.R"))
+source(paste0(wd,"R/00_functions.R"))
+source("/Users/joelpick/github/squidPed/R/simulate_pedigree.R")
+# devtools::load_all("~/github/squidSim/R")
+
+run=FALSE
+
+n_sims <-100
+generations=5
+n_females=100
+fecundity=4
+
+
+#### are number of maternal links the same in the two pedigrees?
+#### number of maternal half sibs will differ presumably
+#### moore et al meta-analysis mentions taking into account pedigree links in the supp matt
+
+## create 100 full sib pedigrees 
+
+peds_param <- rbind(
+fs=c(n_females=n_females,fecundity=fecundity,p_sire = 1,juv_surv = rep(2/fecundity,2),immigration = c(0,0)),
+hs=c(n_females=n_females,fecundity=fecundity,p_sire = 0,juv_surv = rep(2/fecundity,2),immigration = c(0,0)),
+fhs=c(n_females=n_females,fecundity=fecundity,p_sire = 0.75,juv_surv = rep(2/fecundity,2),immigration = c(0,0)),
+
+fs_hF=c(n_females = n_females/2,fecundity = fecundity*2,p_sire = 1,juv_surv = rep(1/fecundity,2),immigration = c(0,0)),
+hs_hF=c(n_females = n_females/2,fecundity = fecundity*2,p_sire = 0,juv_surv = rep(1/fecundity,2),immigration = c(0,0)),
+fhs_hF=c(n_females = n_females/2,fecundity = fecundity*2,p_sire = 0.75,juv_surv = rep(1/fecundity,2),immigration = c(0,0)),
+
+fs_IF=c(n_females = n_females, fecundity = fecundity, p_sire = 1, juv_surv = c(1.5/fecundity,2/fecundity), immigration = c(0.25,0)),
+hs_IF=c(n_females = n_females, fecundity = fecundity, p_sire = 0, juv_surv = c(1.5/fecundity,2/fecundity), immigration = c(0.25,0)),
+fhs_IF=c(n_females = n_females, fecundity = fecundity, p_sire = 0.75, juv_surv = c(1.5/fecundity,2/fecundity), immigration = c(0.25,0)),
+
+fs_IM=c(n_females = n_females, fecundity = fecundity, p_sire = 1, juv_surv = c(2/fecundity,1.5/fecundity), immigration = c(0,0.25)),
+hs_IM=c(n_females = n_females, fecundity = fecundity, p_sire = 0, juv_surv = c(2/fecundity,1.5/fecundity), immigration = c(0,0.25)),
+fhs_IM=c(n_females = n_females, fecundity = fecundity, p_sire = 0.75, juv_surv = c(2/fecundity,1.5/fecundity), immigration = c(0,0.25))
+)
+
+ped_names <- rownames(peds_param)
+
+
+scenarios <- rbind(	
+	# C) Maternal genetic only
+	c=c(Va=0, Vmg=0.4, r_amg=0, Vme=0),
+	# D) Direct genetic and maternal environment
+	e=c(Va=0, Vmg=0.4, r_amg=0, Vme=0.2),#####
+	# F) Direct and maternal genetic, no covariance
+	f=c(Va=0.1, Vmg=0.2, r_amg=0, Vme=0),
+	# I) Direct and maternal genetic, no covariance and maternal environment
+	i=c(Va=0.1, Vmg=0.2, r_amg=0, Vme=0.2)
+)
+
+if(run){
+
+	for(j in 1:nrow(peds_param)){
+		peds <- mclapply(1:n_sims,	function(i){
+			simulate_pedigree(
+				years = generations,
+				n_females = peds_param[j,"n_females"],
+				fecundity = peds_param[j,"fecundity"],
+				p_sire = peds_param[j,"p_sire"],
+				juv_surv = c(peds_param[j,"juv_surv1"],peds_param[j,"juv_surv2"]),
+				adult_surv = 0,					# discrete generations
+				immigration = c(peds_param[j,"immigration1"],peds_param[j,"immigration2"]), 				# closed population
+				constant_pop = TRUE     # constant population size
+				)$pedigree
+		}, mc.cores=8)
+		assign(paste0(ped_names[j] ,"_peds"),peds)	
+	}
+
+
+	for(k in ped_names){
+		dat<-mclapply(get(paste0(k,"_peds")), function(i){
+			x<-list()
+			for(j in 1:nrow(scenarios)){
+				x[[j]]<- mge_sim(i[,1:3], param=scenarios[j,])
+			}
+			x
+		}, mc.cores=8)
+		assign(paste0(k,"_data"),dat)
+	}
+
+	for(k in ped_names){
+		model2 <- model_func(m2_func,get(paste0(k,"_peds")),get(paste0(k,"_data")),mc.cores=8)
+		assign(paste0("model2_",k),model2)
+	}
+
+
+	save(list=(c(paste0("model2_",ped_names))),file=paste0(data_wd,"mge_sims.Rdata"))
+
+}
+
+load(paste0(data_wd,"mge_sims.Rdata"))
+
+for(k in ped_names){
+	mod2 <- do.call(rbind,lapply(get(paste0("model2_",k)), function(x) {
+		do.call(rbind,lapply(1:nrow(scenarios), function(i) data.frame(r=k,scenario=i,comp=c("A","Me"),estimate=x[i,c("A","Me")])))
+	}))
+	assign(paste0("mod2_",k),mod2)
+}
+
+total_Va<-do.call(rbind,lapply(ped_names,function(k) {
+	mod2 <- do.call(rbind,lapply(get(paste0("model2_",k)), function(x) {
+		do.call(rbind,lapply(1:nrow(scenarios), function(i) data.frame(r=k,scenario=i,max=x[i,"A"] + 0.5*x[i,"Me"], min=x[i,"A"] )
+		))
+	}))}
+	))
+total_Va$coverage <- total_Va$max>0.2 & total_Va$min<0.2
+aggregate(coverage~ r+scenario, total_Va, mean)
+
+for(k in ped_names){
+	mod2 <- do.call(rbind,lapply(get(paste0("model2_",k)), function(x) {
+		do.call(rbind,lapply(1:nrow(scenarios), function(i) data.frame(r=k,scenario=i,estimate=sum(x[i,c("A","Me")]))))
+	}))
+	# assign(paste0("mod2_",k),mod2)
+}
+
+# mod2<-rbind(mod2_fs,mod2_hs,mod2_fhs,mod2_fhs_highF,mod2_fs_highF,mod2_fs_I,mod2_fs_IF,mod2_fhs_IF)
+
+cols <- viridis::inferno(6)[c(2,3,5,4,6)]
+
+
+scenarios2 <- scenarios[,c(1,4,2,3)]
+scenarios2[,"r_amg"] <- scenarios2[,"r_amg"]*sqrt(scenarios2[,"Va"])*sqrt(scenarios2[,"Vmg"])
+scenarios2 <- cbind(scenarios2,1 - rowSums(scenarios2) - scenarios2[,"r_amg"])
+colnames(scenarios2) <- c("A","Me","Mg","cov_AMg","E")
+
+s3 <- cbind(scenarios2, 0)[,c(4,6,1,2,3,5)]
+s4<-t(apply(s3,1,function(x) c(x[1], cumsum(x[2:6]))))
+
+ped_n <- length(ped_names)
+
+library(beeswarm)
+par(mfrow=c(2,2),mar=c(4,4,1,1))
+# layout(matrix(c(1:3,0,4:11),nrow=3, byrow=TRUE))
+for(i in 1:4){#nrow(scenarios)
+	beeswarm(estimate~ r, mod2, subset=scenario==i&comp=="A",pch=19, cex=0.2, col=scales::alpha(1,0.3),method = "compactswarm",corral="wrap",  ylim=c(0,0.8),xlim=c(-1,12))#, xlim=c(-1,8)
+	for(j in 1:5){
+		polygon(x=c(-1,0,0,-1),y=c(s4[i,j],s4[i,j],s4[i,j+1],s4[i,j+1]), col=cols[j])	
+	}
+	means<-aggregate(estimate~ r, mod2,mean, subset=scenario==i&comp=="A")$estimate
+
+	arrows((1:(ped_n*2))-0.25,means,(1:(ped_n*2))+0.25, code=0, col="blue")
+	arrows((1:(ped_n*2))-0.25,rep(c(scenarios2[i,1],sum(scenarios2[i,2:3])),each=ped_n),(1:(ped_n*2))+0.25, code=0, col="red")
+
+	# text(1,0.4,paste(paste(colnames(scenarios),"=",scenarios[i,]),collapse=", "),pos=4)
+	
+}
+
+
+
+## correlate means with ratio of maternal link 
+
+
+
+ped_str<-mclapply(ped_names,function(k){
+	ped <- get(paste0(k,"_peds"))[[1]]
+	ped_stat(ped, phenotyped=ped[!is.na(ped[,2]),1])
+},mc.cores=8)
+ped_str<-do.call(cbind,ped_str)
+
+dam
+
+
+
+
--- R/ped_metrics.R
+++ R/ped_metrics.R
@@ -5,14 +5,75 @@ source(paste0(wd,"R/extract_cousins.R"))
 # devtools::install_github("cran/pedantics")
 library(pedantics)
 
+
+
+
 # Difference between blue tit and red deer pedigree
 
 ped_bt <-  MasterBayes::insertPed(read.csv(paste0(wd,"Data/Raw/ped_BT.csv"))[,1:3])
+
+head(ped_bt)
+
 ped_RD<-read.csv(paste0(wd,"Data/Raw/ped_RD.csv"))
 ped_SSH<-read.csv(paste0(wd,"Data/Raw/ped_SSH.csv"))
 ped_SFW<-read.csv(paste0(wd,"Data/Raw/ped_SFW.csv"))
 ped_SV<-read.csv(paste0(wd,"Data/Raw/ped_SV.csv"))
 
+po(ped_RD)
+gp(ped_RD)
+pedSum(ped_RD)
+
+pp<- sample (ped_RD[,1],1500)
+
+cbind(ped_stat(ped_RD),ped_stat(ped_RD,pp))
+
+
+cbind(
+	ped_stat(ped_RD),
+	ped_stat(ped_bt),
+	ped_stat(ped_SSH),
+	ped_stat(ped_SFW),
+	ped_stat(ped_SV)
+	)
+
+ml <- cbind(
+	RD=mat_links(ped_RD),
+	SS=mat_links(ped_SSH),
+	SV=mat_links(ped_SV),
+	SFW=mat_links(ped_SFW),
+	BT=mat_links(ped_bt)
+	)
+
+cov <- read.csv(paste0(wd,"Data/Raw/covariances.csv"))
+
+rd_stat<-ped_stat(ped_RD)
+
+biases <- function(ped,cov){
+	rownames(cov)<-cov$relationship
+	non_me <- c("dam","sire","PHS","MG","PG","au_D_FS","au_S_FS","au_D_MHS","au_S_MHS","au_D_PHS","au_S_PHS","cousin_D_FS","cousin_DS_FS","cousin_S_FS","cousin_D_HS","cousin_DS_HS","cousin_S_HS")
+	
+	stat<-ped_stat(ped)
+
+	c(Vmg = sum(stat[non_me] * cov[non_me,"Vmg"]) / sum(stat[non_me]),
+		COVam = sum(stat[non_me] * cov[non_me,"COVam"]) / sum(stat[non_me]))
+
+}
+## or is it to do with how much Va is as well - so need to multiply by 1/Va cov
+
+biases(ped_bt,cov)
+biases(ped_RD,cov)
+biases(ped_SSH,cov)
+biases(ped_SV,cov)
+biases(ped_SFW,cov)
+
+
+sum(rd_stat[match(cov$relationship,names(rd_stat))] * cov[,"Vmg"])/ sum(rd_stat[match(cov$relationship,names(rd_stat))])
+
+sum(rd_stat[match(cov$relationship,names(rd_stat))] * cov[,"COVam"])/ sum(rd_stat[match(cov$relationship,names(rd_stat))])
+
+
+
+barplot(apply(ml,2,function(x) x/sum(x)))
 
 
 bt_prop <- prop_stat(ped_bt)
@@ -73,6 +134,7 @@ fs_ped <-	simulate_pedigree(
 		constant_pop = TRUE     # constant population size
 		)$pedigree
 
+
 hs_ped <-	simulate_pedigree(
 		years = generations,
 		n_females = n_females,
@@ -97,21 +159,94 @@ fhs_ped <-	simulate_pedigree(
 
 fhs10_ped <- simulate_pedigree(
 		years = generations,
-		n_females = n_females,
-		fecundity = 10,
+		n_females = n_females/2,
+		fecundity = fecundity*2,
 		p_sire = 0.75, 				# mating system (0-1, 1= one male per female, 0=complete random mating)
-		juv_surv = 0.2, # insures no population growth
+		juv_surv = 0.25, # insures no population growth
 		adult_surv = 0,					# discrete generations
 		immigration = 0, 				# closed population
 		constant_pop = TRUE     # constant population size
 		)$pedigree
 
+fs10_ped <-	simulate_pedigree(
+		years = generations,
+		n_females = n_females/2,
+		fecundity = fecundity*2,
+		p_sire = 1, 				# mating system (0-1, 1= one male per female, 0=complete random mating)
+		juv_surv = 0.25, # insures no population growth
+		adult_surv = 0,					# discrete generations
+		immigration = 0, 				# closed population
+		constant_pop = TRUE     # constant population size
+		)$pedigree
+
+fs_ped_I <-	simulate_pedigree(
+		years = generations,
+		n_females = n_females,
+		fecundity = fecundity,
+		p_sire = 1, 				# mating system (0-1, 1= one male per female, 0=complete random mating)
+		juv_surv = 1.5/fecundity, # insures no population growth
+		adult_surv = 0,					# discrete generations
+		immigration = 0.25, 				# closed population
+		constant_pop = TRUE     # constant population size
+		)$pedigree
+
+fs_ped_I2 <-	simulate_pedigree(
+		years = generations,
+		n_females = n_females,
+		fecundity = fecundity,
+		p_sire = 1, 				# mating system (0-1, 1= one male per female, 0=complete random mating)
+		juv_surv = c(1.5/fecundity,2/fecundity), # insures no population growth
+		adult_surv = 0,					# discrete generations
+		immigration = c(0.25,0), 				# closed population
+		constant_pop = TRUE     # constant population size
+		)$pedigree
+
+cbind(
+	mat_links(fs_ped),
+	mat_links(fs10_ped),
+	mat_links(hs_ped),
+	mat_links(fhs_ped),
+	mat_links(fhs10_ped),
+	mat_links(fs_ped_I),
+	mat_links(fs_ped_I2)
+)
 
 
+nrow(fs_ped)
+nrow(hs_ped)
+nrow(fhs_ped)
+## difference in total size of pedigree due to starting number of females - shouldn't be a difference in phenotyped individuals
+nrow(fhs10_ped)
+nrow(fs10_ped)
+nrow(fs_ped_I) ## different in total size of pedigree because there is immigrants and the offspring that leave - shouldn't be a difference in phenotyped individuals
+nrow(fs_ped_I2)
+
+cbind(cousins(fs_ped),
+cousins(hs_ped),
+cousins(fhs_ped),
+cousins(fhs10_ped))
+
+barplot(rbind(prop_stat(fs_ped[,1:3]),
+prop_stat(hs_ped[,1:3]),
+prop_stat(fhs_ped[,1:3]),
+prop_stat(fhs10_ped[,1:3])),beside=TRUE)
+
+##why no cousin_FS_FM in FS design?? there are a_u_FS_FM
+
+library(pedantics)
 fs_stat<-pedigreeStats(fs_ped[,1:3], includeA=FALSE,lowMem=TRUE,graphicalReport=FALSE)
+pedStatSummary(fs_stat)[2:12]
+sum(!is.na(fs_ped[,2]))
+sum(!is.na(fs_ped[,3]))
+gp(fs_ped)
+
+
 hs_stat<-pedigreeStats(hs_ped[,1:3], includeA=FALSE,lowMem=TRUE,graphicalReport=FALSE)
 fhs_stat<-pedigreeStats(fhs_ped[,1:3], includeA=FALSE,lowMem=TRUE,graphicalReport=FALSE)
 fhs10_stat<-pedigreeStats(fhs10_ped[,1:3], includeA=FALSE,lowMem=TRUE,graphicalReport=FALSE)
+fs10_stat<-pedigreeStats(fs10_ped[,1:3], includeA=FALSE,lowMem=TRUE,graphicalReport=FALSE)
+fs_stat_I<-pedigreeStats(fs_ped_I[,1:3], includeA=FALSE,lowMem=TRUE,graphicalReport=FALSE)
+fs_stat_I2<-pedigreeStats(fs_ped_I2[,1:3], includeA=FALSE,lowMem=TRUE,graphicalReport=FALSE)
 
 
 # pedStatSummary(fs_stat)
@@ -128,9 +263,21 @@ cbind(
 
 cbind(
 	pedStatSummary(fs_stat)[2:12],
+	pedStatSummary(fs10_stat)[2:12],
 	pedStatSummary(hs_stat)[2:12],
 	pedStatSummary(fhs_stat)[2:12],
-	pedStatSummary(fhs10_stat)[2:12]
+	pedStatSummary(fhs10_stat)[2:12],
+	pedStatSummary(fs_stat_I)[2:12],
+	pedStatSummary(fs_stat_I2)[2:12]
 )
+
+cbind(cousins(fs_ped),
+	cousins(fs10_ped),
+cousins(hs_ped),
+cousins(fhs_ped),
+cousins(fhs10_ped),
+cousins(fs_ped_I),cousins(fs_ped_I2)
+)
+1
 
 # make fhs10 samller so total sample size is smaller 

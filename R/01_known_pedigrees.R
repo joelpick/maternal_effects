44236266f40b80616174dedab049be048edd146c
Joel Pick <joel.l.pick@gmail.com>
2023-01-20 19:05:42 +0000

    extacting paramters from known pedigrees

---
+++ R/01_known_pedigrees.R
@@ -0,0 +1,101 @@
+rm(list=ls())
+wd <- "/Users/joelpick/github/maternal_effects/"
+
+source(paste0(wd,"R/extract_cousins.R"))
+
+ped_bt <-  MasterBayes::insertPed(read.csv(paste0(wd,"Data/Raw/ped_BT.csv"))[,1:3])
+
+head(ped_bt)
+
+ped_rd<-read.csv(paste0(wd,"Data/Raw/ped_RD.csv"))
+
+par(mfrow=c(2,1))
+plot(table(table(ped_bt$dam)))
+mean(table(ped_bt$dam))
+
+plot(table(table(ped_rd$dam)))
+mean(table(ped_rd$dam))
+
+ped<-ped_rd
+immigration <- function(ped, sex_specific=TRUE){
+	founders <- ped$animal[is.na(ped$sire) & is.na(ped$sire)]
+	ped$dam_founder <- ped$dam%in%founders
+	ped$sire_founder <- ped$sire%in%founders
+	year_d <- aggregate(cohort~dam,subset(ped,!dam%in%founders),min)$cohort
+	year_d_F <- aggregate(cohort~dam,subset(ped,dam%in%founders),min)$cohort
+	year_s <- aggregate(cohort~sire,subset(ped,!sire%in%founders),min)$cohort
+	year_s_F <- aggregate(cohort~sire,subset(ped,sire%in%founders),min)$cohort
+
+	year_d <- aggregate(cohort~dam+dam_founder,ped,min)
+
+	all_d <- table(year_d$cohort,year_d$dam_founder)
+
+	all_s <- table(year_s)
+	all_d_F <- table(year_d_F)
+	all_s_F <- table(year_s_F)
+
+
+	print(all_d)
+	print(all_s)
+
+	c(
+		f=mean(all_d[-1]/all_d[1]),
+		m=mean(all_s[-1]/all_s[1])
+		)
+}
+
+immigration(ped_rd)
+
+plot(as.numeric(table(ped_rd$cohort)))
+
+plot(aggregate(dam~cohort,ped_rd,function(x) length(unique(x)))$dam)
+plot(aggregate(sire~cohort,ped_rd,function(x) length(unique(x)))$sire)
+
+
+
+
+
+
+rm(list=ls())
+
+
+library(asreml)
+library(parallel)
+
+wd <- "/Users/joelpick/github/maternal_effects/"
+
+data_wd <- paste0(wd,"Data/Intermediate/")
+
+source(paste0(wd,"R/extract_cousins.R"))
+source(paste0(wd,"R/00_functions.R"))
+source("/Users/joelpick/github/squidPed/R/simulate_pedigree.R")
+# devtools::load_all("~/github/squidSim/R")
+
+run=FALSE
+
+n_sims <-100
+generations=5
+n_females=100
+fecundity=10
+
+n_females=n_females
+fecundity=fecundity
+p_sire = 1
+p_polyandry=0.5
+juv_surv = rep(2/fecundity,2)
+immigration = c(0,0)
+
+
+ped <- simulate_pedigree(
+	years = generations,
+	n_females = n_females,
+	fecundity = fecundity,
+	p_sire = p_sire,
+	p_polyandry=1,
+	juv_surv = juv_surv,
+	adult_surv = 0,					# discrete generations
+	immigration = immigration, 				# closed population
+	constant_pop = TRUE     # constant population size
+)$pedigree
+
+

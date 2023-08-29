fb3f418764f94fde1dfa593e92bd20c1f1d4939d
Joel Pick <joel.l.pick@gmail.com>
2023-08-29 07:40:48 +0100

    updated simulations

--- R/01_gaussian_data_mge.R
+++ R/01_gaussian_data_mge.R
@@ -79,25 +79,27 @@ scenarios <- rbind(
 	a=c(Va=0, Vmg=0.25, Vme=0, r_amg=0),
 	# B) Direct genetic and maternal environment
 	b=c(Va=0, Vmg=0.25, Vme=0.25, r_amg=0),
-	# C) high Maternal genetic only
-	c=c(Va=0, Vmg=0.5, Vme=0, r_amg=0),
-	# D) high Maternal environment only
-	d=c(Va=0, Vmg=0, Vme=0.5, r_amg=0),
-	# E) Direct and maternal genetic, no covariance
-	e=c(Va=0.25, Vmg=0.25, Vme=0, r_amg=0),
-	# F) Direct and maternal environment, no covariance
-	f=c(Va=0.25, Vmg=0, Vme=0.25, r_amg=0),
-	# G) Direct and maternal genetic, no covariance and maternal environment
-	g=c(Va=0.25, Vmg=0.25, Vme=0.25, r_amg=0),
-	# H) Direct and maternal genetic, + covariance and maternal environment
-	h=c(Va=0.25, Vmg=0.25, Vme=0, r_amg=0.5),
-	# I) Direct and maternal genetic, - covariance and maternal environment
-	i=c(Va=0.25, Vmg=0.25, Vme=0, r_amg=-0.5)
+	# C) Direct and maternal genetic, no covariance
+	c=c(Va=0.25, Vmg=0.25, Vme=0, r_amg=0),
+	# D) Direct and maternal genetic, no covariance and maternal environment
+	d=c(Va=0.25, Vmg=0.25, Vme=0.25, r_amg=0),
+
+	# E) high Maternal genetic only
+	e=c(Va=0, Vmg=0.5, Vme=0, r_amg=0),
+	# F) high Maternal environment only
+	f=c(Va=0, Vmg=0, Vme=0.5, r_amg=0),
+
+	# G+H) Direct and maternal genetic, + covariance and maternal environment
+	g=c(Va=0.25, Vmg=0.25, Vme=0, r_amg=0.6),
+	h=c(Va=0.25, Vmg=0.25, Vme=0, r_amg=0.3),
+	# I+J) Direct and maternal genetic, - covariance and maternal environment
+	i=c(Va=0.25, Vmg=0.25, Vme=0, r_amg=-0.3),
+	j=c(Va=0.25, Vmg=0.25, Vme=0, r_amg=-0.6)
 		
 )
 
 if(run){
-
+	cores<-4
 	set.seed(20230126)
 
 	ped_str <- vector("list",length=nrow(peds_param))
@@ -105,7 +107,7 @@ if(run){
 	ped_str_mat <- vector("list",length=nrow(peds_param))
 	names(ped_str_mat) <- ped_names
 
-# k=ped_names[6]
+# k="fs_lF_mI"
 	## make pedigrees
 	
 	for(k in ped_names){
@@ -123,12 +125,12 @@ if(run){
 				immigration = c(peds_param[k,"immigration_f"],peds_param[k,"immigration_m"]), 				# closed population
 				constant_pop = TRUE     # constant population size
 				)$pedigree
-		}, mc.cores=8)
+		}, mc.cores=cores)
 		# assign(paste0(k ,"_peds"),peds)	
 
 		cat("Generating Pedigree Metrics\n")
-		ped_str[[k]]<- do.call(rbind,mclapply(peds,ped_stat, mc.cores=8))
-		ped_str_mat[[k]]<- do.call(rbind,mclapply(peds,ped_stat2, mc.cores=8))
+		ped_str[[k]]<- do.call(rbind,mclapply(peds,ped_stat, mc.cores=cores))
+		ped_str_mat[[k]]<- do.call(rbind,mclapply(peds,ped_stat2, mc.cores=cores))
 		
 	
 
@@ -141,23 +143,24 @@ if(run){
 				x[[j]]<- mge_sim(i[,1:3], param=scenarios[j,])
 			}
 			x
-		}, mc.cores=8)
+		}, mc.cores=cores)
 		# assign(paste0(k,"_data"),dat)
 
 	## run models
 		cat("Running models: \n")
 	
-		cat("Model 1: ")
-		model1 <- model_func(m1a_func,peds,dat,mc.cores=8)
-		assign(paste0("model1_",k),model1)
+		# cat("Model 1: ")
+		# model1 <- model_func(m1a_func,peds,dat,mc.cores=cores)
+		# assign(paste0("model1_",k),model1)
 		cat("\nModel 2: ")
-		model2 <- model_func(m2_func,peds,dat,mc.cores=8)
+		model2 <- model_func(m2_func,peds,dat,mc.cores=cores)
 		assign(paste0("model2_",k),model2)
 		cat("\n")
 		rm(peds,dat)
 	}
 
-	save(list=(c("ped_names","scenarios","ped_str","ped_str_mat",paste0("model1_",ped_names),paste0("model2_",ped_names))),file=paste0(data_wd,"mge_sims3.Rdata"))
+#paste0("model1_",ped_names),
+	save(list=(c("ped_names","scenarios","ped_str","ped_str_mat",paste0("model2_",ped_names))),file=paste0(data_wd,"mge_sims3.Rdata"))
 }
 
 load(paste0(data_wd,"mge_sims2.Rdata"))

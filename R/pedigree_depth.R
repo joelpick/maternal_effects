590877b0af4110d4e402f24deb479c80b78d24e7
Joel Pick <joel.l.pick@gmail.com>
2023-08-31 20:50:56 +0100

    pedigree depth

--- R/pedigree_depth.R
+++ R/pedigree_depth.R
@@ -10,17 +10,43 @@ data_wd <- paste0(wd,"Data/Intermediate/")
 source("/Users/joelpick/github/squidPed/R/simulate_pedigree.R")
 
 max_gen <- 10
-n_sims <- 100
+n_sims <- 50
 cores<-6
+fecundity=8
+
+immigration <- rbind(
+	no_immigration = c(0,0),
+	f_immigration=c(0.4,0.1),
+	m_immigration=c(0.1,0.4),
+	b_immigration=c(0.25,0.25))
+
+combos<-expand.grid(gens=1:10,immigration=1:4)
+
+peds_param <- cbind(
+	years= combos[,"gens"],
+	immigration_f = immigration[combos[,"immigration"],1],
+	immigration_m = immigration[combos[,"immigration"],2])
+
+peds_param <- cbind(peds_param, 
+	juv_surv_f=2*(1 - peds_param[,"immigration_f"])/fecundity,
+	juv_surv_m=2*(1 - peds_param[,"immigration_m"])/fecundity
+)
+
+# ped_names <- rownames(peds_param) <- paste(
+# 	ms_names[combos[,"p_sire"]],
+# 	f_names[combos[,"fecundity"]],
+# 	i_names[combos[,"immigration"]], sep="_")
+
+
 	ped_str <- vector("list",length=max_gen)
 	
-	for(k in 1:max_gen){
+	for(k in 1:length(peds_param)){
 		cat(k, "\n")
 		ped_str[[k]] <- do.call(rbind,mclapply(1:n_sims,	function(i){
 			ped <- simulate_pedigree(
 				years = k,
 				n_females = 100,
-				fecundity = 8,
+				fecundity = fecundity,
 				p_sire = 0.75,
 				p_polyandry=1,
 				juv_surv = 0.25,

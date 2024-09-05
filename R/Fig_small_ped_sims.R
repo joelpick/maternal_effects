9694cfe0453a8e73aba55187ae8e193403d7390c
Joel Pick <joel.l.pick@gmail.com>
2024-09-05 19:05:40 +0100

    plots

--- R/Fig_small_ped_sims.R
+++ R/Fig_small_ped_sims.R
@@ -61,14 +61,17 @@ all_mod$order <- match(all_mod$r_model,order)
 
 all_mod$Va_bias <- all_mod$Va_est - all_mod$Va_sim
 
+# code above calcaultes a vm also for modelt aht didnt converge - undo this
+all_mod$Vm_est <- ifelse(is.na(all_mod$Va_est),NA,all_mod$Vm_est)
+
 all_mod$Vm_bias <- all_mod$Vm_est - all_mod$Vm_sim
 
 
 
 ## calculation of total Va
 
-all_mod$Vmg_est <- ifelse(is.na(all_mod$Vmg_est),0,all_mod$Vmg_est)
-all_mod$cov_amg_est <- ifelse(is.na(all_mod$cov_amg_est),0,all_mod$cov_amg_est)
+all_mod$Vmg_est <- ifelse(all_mod$model %in% c(1,2) & is.na(all_mod$Vmg_est),0,all_mod$Vmg_est)
+all_mod$cov_amg_est <- ifelse(all_mod$model %in% c(1,2,4) & is.na(all_mod$cov_amg_est),0,all_mod$cov_amg_est)
 
 all_mod$tVa_sim <- all_mod$Va_sim + 0.5*all_mod$Vmg_sim+ 1.5*all_mod$cov_amg_sim
 
@@ -76,7 +79,7 @@ all_mod$tVa_est <- all_mod$Va_est + 0.5*all_mod$Vmg_est+ 1.5*all_mod$cov_amg_est
 
 all_mod$tVa_bias <- all_mod$tVa_est - all_mod$tVa_sim
 
-
+head(subset(all_mod,model==5))
 
 
 
@@ -87,63 +90,80 @@ all_mod$tVa_bias <- all_mod$tVa_est - all_mod$tVa_sim
 
 va<- aggregate(cbind(Va_bias,Vm_bias,tVa_bias) ~ scenario+r_model+r+model, all_mod,mean)
 
+va[,c("Va_precision","Vm_precision","tVa_precision")] <- aggregate(cbind(Va_est,Vm_est,tVa_est) ~ scenario+r_model+r+model, all_mod, function(x)1/sd(x))[,c("Va_est","Vm_est","tVa_est")]
 
-va$Va_precision<- aggregate(Va_est ~ scenario+r_model+r+model, all_mod,function(x)1/sd(x))$Va_est
+va[,c("Va_rel_prec","Vm_rel_prec","tVa_rel_prec")] <- aggregate(cbind(Va_est,Vm_est,tVa_est) ~ scenario+r_model+r+model, all_mod, function(x)mean(x)/sd(x))[,c("Va_est","Vm_est","tVa_est")]
 
-va$Va_rel_prec<- aggregate(Va_est ~ scenario+r_model+r+model, all_mod,function(x)mean(x)/sd(x))$Va_est
-
-va$Va_abs_bias <- aggregate(Va_bias ~ scenario+r_model+r+model, all_mod, function(x) mean(abs(x)))$Va_bias
-
-
-
-va$tVa_bias<- aggregate(tVa_bias ~ scenario+r_model+r+model, all_mod,mean)$tVa_bias
-va$tVa_abs_bias<- aggregate(tVa_bias ~ scenario+r_model+r+model, all_mod, function(x) mean(abs(x)))$tVa_bias
+va[,c("Va_abs_bias","Vm_abs_bias","tVa_abs_bias")] <- aggregate(cbind(Va_bias,Vm_bias,tVa_bias) ~ scenario+r_model+r+model, all_mod, function(x) mean(abs(x)))[,c("Va_bias","Vm_bias","tVa_bias")]
 
 va$order <- match(va$r_model,order)
 
 
 
+####
+#  mean difference in absolute error across all simulations  
+####
+mod2 <- subset(all_mod,model==2)
+mod4 <- subset(all_mod,model==4)
+mod5 <- subset(all_mod,model==5)
 
+## difference 
+va_4_5 <- tapply(abs(mod4$Va_bias) - abs(mod5$Va_bias), list(mod2$r_model,mod2$scenario),mean, na.rm=TRUE)
+
+vm_4_5 <- tapply(abs(mod4$Vm_bias) - abs(mod5$Vm_bias), list(mod2$r_model,mod2$scenario),mean, na.rm=TRUE)
+
+tva_4_5 <- tapply(abs(mod4$tVa_bias) - abs(mod5$tVa_bias), list(mod2$r_model,mod2$scenario),mean, na.rm=TRUE)
+
+
+va_2_4 <- tapply(abs(mod2$Va_bias) - abs(mod4$Va_bias), list(mod2$r_model,mod2$scenario),mean, na.rm=TRUE)
+
+vm_2_4 <- tapply(abs(mod2$Vm_bias) - abs(mod4$Vm_bias), list(mod2$r_model,mod2$scenario),mean, na.rm=TRUE)
+
+tva_2_4 <- tapply(abs(mod2$tVa_bias) - abs(mod4$tVa_bias), list(mod2$r_model,mod2$scenario),mean, na.rm=TRUE)
 
 
 ### 
-pchs <- c(15,16,17)
-{
-	par(mfrow=c(4,1),mar=c(1,5,1,1))
-beeswarm(Va_bias~ order, va,pch=pchs, cex=1, col=rep(alpha(palette.colors()[1:4],0.5), each=4),method = "compactswarm",corral="wrap", ylab="Bias in Va", las=2)#, 
-abline(h=0)
-beeswarm(Va_precision~ order, va,pch=pchs, cex=1, col=rep(alpha(palette.colors()[1:4],0.5), each=4),method = "compactswarm",corral="wrap", ylab="Precision Va", las=2)#, 
-beeswarm(Va_rel_prec~ order, va,pch=pchs, cex=1, col=rep(alpha(palette.colors()[1:4],0.5), each=4),method = "compactswarm",corral="wrap", ylab="Relative precision Va", las=2)#, 
-beeswarm(Va_abs_bias~ order, va,pch=pchs, cex=1, col=rep(alpha(palette.colors()[1:4],0.5), each=4),method = "compactswarm",corral="wrap", ylab="Absolute Error Va", las=2)#, label=order)
+
+hl_hist <- function(x, breaks, main="", col1, col2,...){
+	ylim=range(hist(x,plot=FALSE,breaks=breaks)$counts)
+	hist(x[x>=0], breaks=breaks, main=main, col=scales::alpha(col1,0.8),ylim=ylim,...)
+	hist(x[x<0], breaks=breaks, main=main, col=scales::alpha(col2,0.8), add=TRUE)
 }
-
-
-beeswarm(tVa_bias~ order, va,pch=pchs, cex=1, col=rep(alpha(palette.colors()[1:4],0.5), each=4),method = "compactswarm",corral="wrap", ylab="bias in tVa", las=2)#, label=order)
-abline(h=0)
-beeswarm(tVa_abs_bias~ order, va,pch=pchs, cex=1, col=rep(alpha(palette.colors()[1:4],0.5), each=4),method = "compactswarm",corral="wrap", ylab="bias in tVa", las=2)#, label=order)
-abline(h=0)
 
 
 pchs <- c(21:24)
 cols <- alpha(palette.colors()[1:4],0.5)
 
 plot_va <- subset(va, r %in% ped_names_reduced[grep("bI",ped_names_reduced)])
+
+breaks=seq(-0.2,0.2,0.02)
 {
-	par(mfrow=c(4,1),mar=c(4,5,1,1))
-beeswarm(Va_bias~ order, plot_va,pch=pchs, cex=1, col=cols,bg=cols,method = "compactswarm",corral="wrap", xlab='Model',ylab="Bias in Va", label=c(1:4,1:4))#, 
-abline(h=0)
-beeswarm(Va_precision~ order, plot_va,pch=pchs, cex=1, col=cols,bg=cols,method = "compactswarm",corral="wrap", xlab='Model',ylab="Precision Va", label=c(1:4,1:4))#, 
-beeswarm(Va_rel_prec~ order, plot_va,pch=pchs, cex=1, col=cols,bg=cols,method = "compactswarm",corral="wrap", xlab='Model',ylab="Realtive precision Va", label=c(1:4,1:4))#, 
+	par(mar=c(4,5,3,1))
+
+	layout(matrix(c(1,1,2,3,4,4,5,6,7,7,8,9),ncol=4,byrow=TRUE))
 beeswarm(Va_abs_bias~ order, plot_va,pch=pchs, cex=1, col=cols,bg=cols,method = "compactswarm",corral="wrap", xlab='Model',ylab="Absolute Error Va", label=c(1:4,1:4))#, label=order)
+hl_hist(as.vector(va_2_4), breaks=breaks, col1=palette.colors()[3],col2=palette.colors()[2],  main ="Model 2 - Model 3", xlab="Difference")
+hl_hist(va_4_5, breaks=breaks, main ="Model 3 - Model 4", xlab="Difference", col1=palette.colors()[4],col2=palette.colors()[3])
+
+
+beeswarm(Vm_abs_bias~ order, plot_va,pch=pchs, cex=1, col=cols,bg=cols,method = "compactswarm",corral="wrap", xlab='Model',ylab="Absolute Error Vm", label=c(1:4,1:4))#, label=order)
+hl_hist(vm_2_4, breaks=breaks, col1=palette.colors()[3],col2=palette.colors()[2], main ="Model 2 - Model 3", xlab="Difference")
+hl_hist(vm_4_5, breaks=breaks, main ="Model 3 - Model 4", xlab="Difference", col1=palette.colors()[4],col2=palette.colors()[3])
+
+beeswarm(tVa_abs_bias~ order, plot_va,pch=pchs, cex=1, col=cols,bg=cols,method = "compactswarm",corral="wrap", xlab='Model',ylab="Absolute Error total Va", label=c(1:4,1:4))#, label=order)
+hl_hist(tva_2_4, breaks=breaks, col1=palette.colors()[3],col2=palette.colors()[2], main ="Model 2 - Model 3", xlab="Difference")
+hl_hist(tva_4_5, breaks=breaks, main ="Model 3 - Model 4", xlab="Difference", col1=palette.colors()[4],col2=palette.colors()[3])
 }
+
+
 
 	par(mfrow=c(1,1),mar=c(4,5,1,1))
 
-s_col <- rep(alpha(palette.colors()[1:4],0.5), 8)[va$order]
-plot(Va_abs_bias~ order, va,pch=pchs, cex=1, col=s_col, bg=s_col ylab="Mean Absolute Error in Va", las=2)#, 
+s_col <- rep(alpha(palette.colors()[1:4],0.5), 8)[plot_va$order]
+plot(Va_abs_bias~ order, plot_va,pch=pchs, cex=1, col=s_col, bg=s_col, ylab="Mean Absolute Error in Va", las=2)#, 
 for(i in 1:nrow(scenarios)){
 	for(j in 1:length(ped_names_reduced)){
-	lines(Va_abs_bias~order,subset(va,r==ped_names_reduced[j] & scenario==i), col=alpha(1,0.2))}}
+	lines(Va_abs_bias~order,subset(plot_va,r==ped_names_reduced[j] & scenario==i), col=alpha(1,0.2))}}
 
 {
 	breaks=seq(-0.2,0.2,0.02)
@@ -156,8 +176,8 @@ par(mar=c(4,4,1,1))
 
 ## mod 4 better than mod 3
 hist(subset(va, model==2)$Va_abs_bias-subset(va, model==4)$Va_abs_bias, breaks=breaks)
-hist(subset(va, model==2 & scenario%in%c(3,7:12))$Va_abs_bias-subset(va, model==4& scenario%in%c(3,7:12))$Va_abs_bias, breaks=breaks, add=TRUE, col="red")
-hist(subset(va, model==2 & scenario%in%c(3,11,12))$Va_abs_bias-subset(va, model==4& scenario%in%c(3,11,12))$Va_abs_bias, breaks=breaks, add=TRUE, col="blue")
+# hist(subset(va, model==2 & scenario%in%c(3,7:12))$Va_abs_bias-subset(va, model==4& scenario%in%c(3,7:12))$Va_abs_bias, breaks=breaks, add=TRUE, col="red")
+# hist(subset(va, model==2 & scenario%in%c(3,11,12))$Va_abs_bias-subset(va, model==4& scenario%in%c(3,11,12))$Va_abs_bias, breaks=breaks, add=TRUE, col="blue")
 
 
 ## mod 5 better than mod 4
